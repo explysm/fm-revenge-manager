018a2ac5daf39ba3842ae13a2219baeea3a5ec18
explysm <joshualtrost@gmail.com>
2026-01-15 21:10:46 -0600

    fix build issues

--- app/src/main/java/app/revenge/manager/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/app/revenge/manager/ui/screen/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package app.revenge.manager.ui.screen.home
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
