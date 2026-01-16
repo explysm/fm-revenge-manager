75d1af157f50c0a83255db5ffc86f60a8f56b789
explysm <joshualtrost@gmail.com>
2026-01-15 21:49:23 -0600

    why do i exist?

--- app/src/main/java/app/revenge/manager/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/app/revenge/manager/ui/screen/home/HomeScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
